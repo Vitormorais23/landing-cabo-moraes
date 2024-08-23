60423a1fac4aa8dfb22c397717c344dc307a9f0b
José Vitor Nunes de Morais <josevitornm2014@outlook.com>
2024-08-23 09:54:04 -0300

    Corração do toTop que estava no mobile

--- js/move-top.js
+++ js/move-top.js
@@ -2,12 +2,56 @@
 
 (function ($) {
     $.fn.UItoTop = function (options) {
-        var defaults = { text: 'To Top', min: 200, inDelay: 600, outDelay: 400, containerID: 'toTop', containerHoverID: 'toTopHover', scrollSpeed: 1000, easingType: 'linear' }, settings = $.extend(defaults, options), containerIDhash = '#' + settings.containerID, containerHoverIDHash = '#' + settings.containerHoverID; $('body').append('<a href="#" id="' + settings.containerID + '">' + settings.text + '</a>'); $(containerIDhash).hide().on('click.UItoTop', function () { $('html, body').animate({ scrollTop: 0 }, settings.scrollSpeed, settings.easingType); $('#' + settings.containerHoverID, this).stop().animate({ 'opacity': 0 }, settings.inDelay, settings.easingType); return false; }).prepend('<span id="' + settings.containerHoverID + '"></span>').hover(function () { $(containerHoverIDHash, this).stop().animate({ 'opacity': 1 }, 600, 'linear'); }, function () { $(containerHoverIDHash, this).stop().animate({ 'opacity': 0 }, 700, 'linear'); }); $(window).scroll(function () {
-            var sd = $(window).scrollTop(); if (typeof document.body.style.maxHeight === "undefined") { $(containerIDhash).css({ 'position': 'absolute', 'top': sd + $(window).height() - 50 }); }
-            if (sd > settings.min)
-                $(containerIDhash).fadeIn(settings.inDelay); else
-                $(containerIDhash).fadeOut(settings.Outdelay);
+        var defaults = { 
+            text: 'To Top', 
+            min: 200, 
+            inDelay: 600, 
+            outDelay: 400, 
+            containerID: 'toTop', 
+            containerHoverID: 'toTopHover', 
+            scrollSpeed: 1000, 
+            easingType: 'linear' 
+        }, 
+        settings = $.extend(defaults, options), 
+        containerIDhash = '#' + settings.containerID, 
+        containerHoverIDHash = '#' + settings.containerHoverID;
+
+        $('body').append('<a href="#" id="' + settings.containerID + '">' + settings.text + '</a>');
+
+        $(containerIDhash).hide().on('click.UItoTop', function () { 
+            $('html, body').animate({ scrollTop: 0 }, settings.scrollSpeed, settings.easingType); 
+            $('#' + settings.containerHoverID, this).stop().animate({ 'opacity': 0 }, settings.inDelay, settings.easingType); 
+            return false; 
+        }).prepend('<span id="' + settings.containerHoverID + '"></span>').hover(function () { 
+            $(containerHoverIDHash, this).stop().animate({ 'opacity': 1 }, 600, 'linear'); 
+        }, function () { 
+            $(containerHoverIDHash, this).stop().animate({ 'opacity': 0 }, 700, 'linear'); 
         });
+
+        $(window).scroll(function () {
+            var sd = $(window).scrollTop(); 
+
+            // Verifica a largura da tela
+            if ($(window).width() <= 667) {
+                $(containerIDhash).hide(); // Esconde o botão se a tela for >= 667px
+                return; // Sai da função para evitar a execução das linhas abaixo
+            } 
+
+            if (typeof document.body.style.maxHeight === "undefined") { 
+                $(containerIDhash).css({ 'position': 'absolute', 'top': sd + $(window).height() - 50 }); 
+            }
+
+            if (sd > settings.min)
+                $(containerIDhash).fadeIn(settings.inDelay); 
+            else
+                $(containerIDhash).fadeOut(settings.outDelay);
+        });
+
+        // Verifica a largura da tela quando a página é carregada
+        if ($(window).width() <= 667) {
+            $(containerIDhash).hide(); // Esconde o botão se a tela for >= 667px
+        }
+
     };
 })(jQuery);
 
